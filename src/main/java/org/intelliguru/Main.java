1675000c62b2344713df1855965fb056e34d9ffd
KRISHNA TRIPATHI <bktripathi1977@gmail.com>
2025-01-05 23:01:19 +0530

    Solved 4th problem "Valid Parenthesis"

--- src/main/java/org/intelliguru/Main.java
+++ src/main/java/org/intelliguru/Main.java
@@ -2,8 +2,7 @@ package org.intelliguru;
 
 public class Main {
     public static void main(String[] args) {
-        LongestCommonPrefixes lcp = new LongestCommonPrefixes();
-        String[] words = {"flower","flow","flight"};
-        String result = lcp.longestCommonPrefixV2(words);
+        ValidParenthesis vp = new ValidParenthesis();
+        boolean result = vp.validParenthesis("[{()}]");
     }
 }
