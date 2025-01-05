a4f074ce2172675f074458c40f8cf33eec185279
KRISHNA TRIPATHI <bktripathi1977@gmail.com>
2025-01-05 19:02:02 +0530

    Solved 2nd problem "RomanToInt" converson

--- src/main/java/org/intelliguru/Main.java
+++ src/main/java/org/intelliguru/Main.java
@@ -2,8 +2,7 @@ package org.intelliguru;
 
 public class Main {
     public static void main(String[] args) {
-        int[] nums = {1,3,5};
-        TwoSum twoSum = new TwoSum();
-        twoSum.twoSum(nums, 8);
+        RomanInteger roman = new RomanInteger();
+        roman.romanToInt("III");
     }
 }
